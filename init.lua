68c150b7245b260b0699c55a63575b3e41700cad
Bison Hubert <bisonhubert@gmail.com>
2025-08-28 13:07:24 -0700

    Prettify format on keymap

--- init.lua
+++ init.lua
@@ -225,6 +225,13 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Format current file only with prettier
+vim.keymap.set('n', '<leader>p', function()
+  local filepath = vim.fn.expand('%:p')
+  vim.fn.system('yarn prettier --write ' .. vim.fn.shellescape(filepath))
+  vim.cmd('checktime') -- Reload buffer to show changes
+end, { desc = 'Format current file with prettier' })
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
